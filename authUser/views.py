0aa9185e2d32ab8c8e6f7bfd9e10a233c92d4e00
Fred Herbert <44193167+Freditto@users.noreply.github.com>
2023-07-09 14:36:56 +0300

    Update views.py

--- authUser/views.py
+++ authUser/views.py
@@ -58,11 +58,16 @@ def LoginView(request):
             profile = True
             x = Profile.objects.filter(user=User.objects.get(username=username))[0]
             bmis = Bmi.objects.all()
+            print('BMI OF USER', x.bmi)
+            bmi_name = None
             for d in bmis:
-                if d.min_range <= x.bmi <= d.max_range:
-                    bmi_name = d.name
+                if bmi_name is None:
+                    if d.min_range <= x.bmi <= d.max_range:
+                        bmi_name = d.name
                 else:
-                    bmi_name = "Over weight"
+                    break
+            if bmi_name is None:
+                bmi_name = "Overweight"
 
             profile_data = {
                 'id': x.id,
